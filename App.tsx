a406e1870647002411ea26bb86a0c6a7a7dc2098
Javier <jalcoser1996@gmail.com>
2021-04-22 19:21:43 -0500

    Crear stack
    Instalar y crear paginas el stackNavigator

--- App.tsx
+++ App.tsx
@@ -2,13 +2,15 @@ import 'react-native-gesture-handler';
 import React from 'react'
 import { Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
+import { StackNavigator } from './src/navigator/StackNavigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <Text>
+      {/* <Text>
         Pantalla de navegación
-      </Text>
+      </Text> */}
+      <StackNavigator></StackNavigator>
     </NavigationContainer>
   )
 }
