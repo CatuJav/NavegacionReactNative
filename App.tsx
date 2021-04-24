2f51900b8a6f679bd3cb3e8f63e309d3846e221d
Javier <jalcoser1996@gmail.com>
2021-04-23 23:20:49 -0500

    Configurar Drawer básico

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './src/navigator/StackNavigator';
+import { MenuLateralBasico } from './src/navigator/MenuLateralBasico';
 
 export const App = () => {
   return (
@@ -10,7 +11,10 @@ export const App = () => {
       {/* <Text>
         Pantalla de navegación
       </Text> */}
-      <StackNavigator></StackNavigator>
+      {/* <StackNavigator></StackNavigator> */}
+      
+      {/**Ahora ya no llamamos al menu lateral primero  */}
+      <MenuLateralBasico/>
     </NavigationContainer>
   )
 }
