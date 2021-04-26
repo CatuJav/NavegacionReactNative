0c02fda36de16c8ee9f5e7ae62fb4022dcae86ed
Javier <jalcoser1996@gmail.com>
2021-04-25 21:19:56 -0500

    Preparar el espacio para el ícono
    Se usa funcion y un switch para cada una de las rutas de la pantalla

--- src/navigator/Tabs.tsx
+++ src/navigator/Tabs.tsx
@@ -4,7 +4,8 @@ import { Tab1Screen } from '../screens/Tab1Screen';
 import { Tab2Screen } from '../screens/Tab2Screen';
 import { Tab3Screen } from '../screens/Tab3Screen';
 import { StackNavigator } from './StackNavigator';
-import { colores } from '../theme/appTheme';
+import { colores, styles } from '../theme/appTheme';
+import { Text } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -21,11 +22,46 @@ export const Tabs=()=> {
         style:{borderTopColor:colores.primary,
                borderTopWidth:0,
                elevation:0
-              }
+              },
+        labelStyle:{
+          fontSize:15,
+        }
       }}
       //
+      screenOptions={
+        ({route})=>(
+         {
+           tabBarIcon:({color,focused,size})=>{
+             //console.log(route.name)
+             let iconName:string='';
+             switch (route.name) {
+               case 'Tab1Screen':
+                 iconName='T1'
+                 break;
+               case 'Tab2Screen':
+                  iconName='T2'
+                  break;
+                  case 'StackNavigator':
+                    iconName='St'
+                    break;
+                 
+               
+             }
+             return <Text style={{color}}>{iconName}</Text>
+
+           }
+        } 
+        )
+      }
     >
-      {/**Con las opcionts puedo cambiar el nombre de los tabs que se muestran */}
+      {/**Con las opcionts puedo cambiar el nombre de los tabs que se muestran.
+       * tabBarIcon se pide una funcion que devuelva nodo reactjsx
+       */}
+       {/**Ejemplo de como poner iconos en el bottomBar */}
+      {/* <Tab.Screen name="Tab1Screen" options={{title:'Tab 1',tabBarIcon:(props)=>(
+        <Text style={{color:props.color}}>T1</Text>
+      )}} component={Tab1Screen} /> */}
+
       <Tab.Screen name="Tab1Screen" options={{title:'Tab 1'}} component={Tab1Screen} />
       <Tab.Screen name="Tab2Screen" options={{title:'Tab 2'}} component={Tab2Screen} />
       <Tab.Screen name="StackNavigator" options={{title:'Stack'}} component={StackNavigator} />
