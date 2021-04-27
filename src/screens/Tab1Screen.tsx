2ba10d30bf0821c9287edb60b3fddae8468a4b9d
Javier <jalcoser1996@gmail.com>
2021-04-26 23:11:26 -0500

    Instalación y configuración de paquetes de iconos
    Se instala y configura el paquete de iconos y se muestra un ejemplo en tab1Screen

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,16 +1,29 @@
 import React, { useEffect } from 'react'
 import { Text, View } from 'react-native';
+import { styles, colores } from '../theme/appTheme';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const Tab1Screen = () => {
-
+ 
+    const {top}=useSafeAreaInsets()
     useEffect(() => {
         console.log('Tabs 1 effect')
         
     }, [])
 
     return (
-        <View>
-            <Text>TabScreen1</Text>
+        <View style={{...styles.globalMargin, marginTop:top+10}}>
+            <Text>Iconos</Text>
+            <Text>
+            <Icon name="flight-land" size={80} color={colores.primary} />
+            <Icon name="shopping-cart" size={80} color={colores.primary} />
+            <Icon name="verified" size={80} color={colores.primary} />
+            <Icon name="https" size={80} color={colores.primary} />
+            <Icon name="view-module" size={80} color={colores.primary} />
+            <Icon name="pivot-table-chart" size={80} color={colores.primary} />
+            <Icon name="work-outline" size={80} color={colores.primary} />
+            </Text>
         </View>
     )
 }
