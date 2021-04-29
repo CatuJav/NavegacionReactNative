6533eab0966442cb50f7ebe1cc7bf3e9a7c95e99
Javier <jalcoser1996@gmail.com>
2021-04-28 21:57:49 -0500

    Consumir el context- AuthState

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Text, View } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { styles } from '../theme/appTheme';
+import { AuthContext } from '../context/AuthContext';
 
 interface Props extends StackScreenProps<any,any>{}
 export const SettingsScreen = ({navigation}:Props) => {
@@ -16,6 +17,10 @@ export const SettingsScreen = ({navigation}:Props) => {
 
     {/**Tener un mejor control de los lugares donde no puedo mostrar contenido */}
     const insets=useSafeAreaInsets();
+
+    //Usar el context
+    const {authState} = useContext(AuthContext);
+
     return (
         //insets permite ajustar como puedo mostrar el contenido 
         //Permitiendo tener controlado el notch
@@ -23,6 +28,9 @@ export const SettingsScreen = ({navigation}:Props) => {
             {...styles.globalMargin,
             marginTop:insets.top+20}}>
             <Text style={styles.title}>SettingsScreen</Text>
+            <Text>
+                {JSON.stringify(authState,null,4)}
+            </Text>
         </View>
     )
 }
